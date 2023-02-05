0c08eb28e4fe7838c06d4a44bacc777f60cb322f
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-05 17:04:56 +0100

    feature: ajouter la validation au form

--- src/app/popup-creer-todo/popup-creer-todo.component.ts
+++ src/app/popup-creer-todo/popup-creer-todo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { TodoModel } from '../models/todo-model';
 import { ResetService } from '../services/reset-service';
 import { TodoService } from '../services/todo-service';
@@ -15,7 +15,7 @@ export class PopupCreerTodoComponent implements OnInit {
 
   constructor(private resetService:ResetService, private todoService:TodoService) { 
     this.fg = new FormGroup({
-      titre: new FormControl(),
+      titre: new FormControl("", [Validators.required]),
       description: new FormControl()
     })
    }
@@ -27,7 +27,12 @@ export class PopupCreerTodoComponent implements OnInit {
     this.resetService.resetEcran();
   }
 
+  getTitreFC(){
+    return this.fg.get("titre");
+  }
+
   sauvegarder(){
+    if(this.fg.touched && this.fg.valid){
     const titre = this.fg.controls["titre"].value;
     const description = this.fg.controls["description"].value;
     console.log(titre);
@@ -35,6 +40,7 @@ export class PopupCreerTodoComponent implements OnInit {
     const todo = new TodoModel(titre, description);
     this.todoService.creerUneTodo(todo);
     this.quitterEcran();
+    }
   }
 
 }
