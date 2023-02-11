7343351f1fdd038f701ef7f61947f58a481ac1d4
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-11 15:15:25 +0100

    refactor: refactor le chemin de la route pour afficher les listes de todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { DetailTodoComponent } from './detail-todo/detail-todo.component';
 import { ToutesTodoComponent } from './toutes-todo/toutes-todo.component';
 
 const routes: Routes = [
-  {path: 'todo/toutes', component:ToutesTodoComponent},
-  {path: '', redirectTo: 'todo/toutes', pathMatch: 'full'},
+  {path: 'todo/listes', component:ToutesTodoComponent},
+  {path: '', redirectTo: 'todo/listes', pathMatch: 'full'},
   {path: 'todo/:id', component: DetailTodoComponent}
 ];
 
