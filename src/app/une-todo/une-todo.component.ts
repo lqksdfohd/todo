ae3b03c1e5f31172b48d36fe18ef2a6e3fac6cc6
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-11 14:03:20 +0100

    feature: on doit pouvoir naviguer vers le detail d'une todo à partir de son id

--- src/app/une-todo/une-todo.component.ts
+++ src/app/une-todo/une-todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TodoModel } from '../models/todo-model';
 import { TodoService } from '../services/todo-service';
 
@@ -12,7 +13,7 @@ export class UneTodoComponent implements OnInit {
   @Input()
   todo:TodoModel;
   
-  constructor(private todoService:TodoService) { }
+  constructor(private todoService:TodoService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -21,4 +22,7 @@ export class UneTodoComponent implements OnInit {
     this.todoService.crossATodo(this.todo);
   }
 
+  naviguerVersDetail(){
+    this.router.navigate(['todo', this.todo.id]);
+  }
 }
