de526c6fe71f950348feba37a2a5ffd7ad39f7c9
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-11 13:47:23 +0100

    refactor: supprimer la logique dans le composant principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observer } from 'rxjs';
-import { TodoModel } from './models/todo-model';
-import { TodoService } from './services/todo-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  currentTodoList: TodoModel[];
-  crossedTodoList: TodoModel[];
-
-  constructor(private todoService: TodoService) { }
-
-  ngOnInit(): void {
-    this.currentTodoList = this.todoService.getListeTodo();
-
-    const observerCurrentList:Observer<TodoModel[]> = {
-      next: (todoList) => {
-        this.currentTodoList = todoList;
-      },
-      error: (error) => {},
-      complete: () => {}
-    }
-
-    const observerCrossedTodoList:Observer<TodoModel[]> = {
-      next: (todoList) => {this.crossedTodoList = todoList},
-      error: (error) => {},
-      complete: () => {}
-    }
-
-    this.todoService.addObserverToCurrentList(observerCurrentList);
-    this.todoService.addObserverToCrossedList(observerCrossedTodoList);
-  }
-
-
+export class AppComponent{
 
 }
