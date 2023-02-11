2f36645f0ca22126ccf06f0370e3585241d9503c
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-11 13:52:22 +0100

    feature: créer la route pour accéder au composant detail-todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailTodoComponent } from './detail-todo/detail-todo.component';
 import { ToutesTodoComponent } from './toutes-todo/toutes-todo.component';
 
 const routes: Routes = [
   {path: 'todo/toutes', component:ToutesTodoComponent},
-  {path: '', redirectTo: 'todo/toutes', pathMatch: 'full'}
+  {path: '', redirectTo: 'todo/toutes', pathMatch: 'full'},
+  {path: 'todo/:id', component: DetailTodoComponent}
 ];
 
 @NgModule({
