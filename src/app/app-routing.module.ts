9c4942e910f1466b869f6b7d092fe4c1666c21de
lqksdfohd <22824559+lqksdfohd@users.noreply.github.com>
2023-02-12 12:26:23 +0100

    feature: créer la route vers le composant creer-todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreerTodoComponent } from './creer-todo/creer-todo.component';
 import { DetailTodoComponent } from './detail-todo/detail-todo.component';
 import { ToutesTodoComponent } from './toutes-todo/toutes-todo.component';
 
 const routes: Routes = [
   {path: 'todo/listes', component:ToutesTodoComponent},
   {path: '', redirectTo: 'todo/listes', pathMatch: 'full'},
-  {path: 'todo/:id', component: DetailTodoComponent}
+  {path: 'todo/creer', component: CreerTodoComponent},
+  {path: 'todo/:id', component: DetailTodoComponent},
+  
 ];
 
 @NgModule({
